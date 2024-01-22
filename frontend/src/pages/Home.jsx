115b95c526ae5f7b4f2d7a95b27e8ceaf6bb1092
xdd <xdd@me.com>
2024-01-22 17:01:16 -0500

    some basic css elements with mui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Box, CssBaseline } from "@mui/material"
+import { margin } from "@mui/system";
 
 const Home = () => {
     return (
-        <>
-            <p>Home</p>
-        </>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <CssBaseline />
+            Home
+        </Box>
     );
 };
 
