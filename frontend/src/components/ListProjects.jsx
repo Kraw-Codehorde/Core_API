b1120cd9a7f880a5081b3b107c58976fe9aa52b4
xdd <xdd@me.com>
2024-01-25 11:57:28 -0500

    basic axios implementation

--- frontend/src/components/ListProjects.jsx
+++ frontend/src/components/ListProjects.jsx
@@ -1,16 +1,19 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import React from "react";
+import { Grid, Button } from "@mui/material";
+import { Box } from "@mui/system";
 
 const ListProjects = (props) => {
-    const { projects } = props
+  const { projects } = props;
 
   return (
     <Grid container spacing={2}>
-        {projects.map((project) => (
-            <Grid item xs={12} sm={6} md={4} key={project.id} />
-        ))}
+      {projects.map((project) => (
+        <Grid item xs={12} sm={6} md={4} key={project.id}>
+          <Box sx={{ p: 2 }}>{project.name}</Box>
+        </Grid>
+      ))}
     </Grid>
-  )
-}
+  );
+};
 
-export default ListProjects
+export default ListProjects;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,70 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, Box, Typography, Button } from "@mui/material";
 import ListProjects from "../components/ListProjects";
-
-const mockProjects = [{
-    id: 1,
-    name: "Project 1",
-    description: "Project 1 description",
-    image: "https://picsum.photos/id/1015/200/300",
-    link: "https://project1.com",
-    github: "https://github.com/project1"
-    },
-    {id: 2,
-    name: "Project 2",
-    description: "Project 2 description",
-    image: "https://picsum.photos/id/1015/200/300",
-    link: "https://project2.com",
-    github: "https://github.com/project2"
-    },
-    {
-        id: 3,
-        name: "Project 3",
-        description: "Project 3 description",
-        image: "https://picsum.photos/id/1015/200/300",
-        link: "https://project3.com",
-        github: "https://github.com/project3"
-    }
-]
+import axios from "axios";
+import { useState } from "react";
 
 const Home = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/projects/")
+      .then((res) => {
+        console.log(res.data);
+        setProjects(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Grid>
-    <Box sx={{ p: 4 }}>
-      {/* Header */}
-      <Box sx={{ mb: 4 }}>
-        <Typography variant="h4">John Doe</Typography>
-        <Typography>Software Engineer</Typography>
+      <Box sx={{ p: 4 }}>
+        {/* Header */}
+        <Box sx={{ mb: 4 }}>
+          <Typography variant="h4">John Doe</Typography>
+          <Typography>Software Engineer</Typography>
+        </Box>
+
+        {/* About section */}
+        <Box sx={{ mb: 4 }}>
+          <Typography variant="h5">About</Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            euismod, nisl vel tincidunt lacinia, nunc nisl aliquam nisl, eu
+            aliquam nisl nisl eu nisl.
+          </Typography>
+        </Box>
+
+        {/* Projects section */}
+        <Box sx={{ mb: 4 }}>
+          <Typography variant="h5">Projects</Typography>
+
+          <Grid container spacing={2}>
+            <ListProjects projects={projects} />
+          </Grid>
+        </Box>
+
+        {/* Contact button */}
+        <Button variant="contained">Contact</Button>
       </Box>
-      
-      {/* About section */}
-      <Box sx={{ mb: 4 }}>
-        <Typography variant="h5">About</Typography>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Sed euismod, nisl vel tincidunt lacinia, nunc nisl aliquam nisl, 
-          eu aliquam nisl nisl eu nisl.
-        </Typography>
-      </Box>
-
-      
-      
-
-{/* Projects section */}
-<Box sx={{ mb: 4 }}>
-  <Typography variant="h5">Projects</Typography>
-
-  <Grid container spacing={2}> 
-    <ListProjects projects={mockProjects} />
-  </Grid>
-</Box>
-
-
-
-      
-      {/* Contact button */}
-      <Button variant="contained">Contact</Button>
-    </Box>
     </Grid>
   );
 };
