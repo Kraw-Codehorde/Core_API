7abd4bb008a021462f354e9db155ca8dba2b72a9
xdd <xdd@xdd.com>
2024-03-07 17:49:06 -0500

    removed image field from project model

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='projects', null=True, blank=True)
+    image = models.URLField(null=True, blank=True)
     url = models.URLField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
